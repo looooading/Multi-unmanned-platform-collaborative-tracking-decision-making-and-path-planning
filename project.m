12c1af36a9c1f11b8c69b258e6e81774dabaacb7
looooading <1275318444@qq.com>
2024-03-28 15:30:46 +0800

    完善适应度函数cost1。其中修正了ally的rand（）函数生成范围，添加详细注释，增加可读性。目前仍有优化空间：位置cost与价值cost的占比仍需考虑，即line 39 中enemy（a，3）前或许要乘一个系数。

--- project.m
+++ project.m
@@ -4,27 +4,30 @@ Em2 = [-0.1,0.1,0.2];
 Em3 = [-0.1,-0.1,0.1];
 Em4 = [0.1,-0.1,0.2];
 Em5 = [0,0,0.3];
-enemy = [Em1;Em2;Em3;Em4;Em5];
-Em1_pos = enemy(1,1:2);
+enemy = [Em1;Em2;Em3;Em4;Em5];%上述生成的敌人信息竖直叠放
+Em1_pos = enemy(1,1:2);%enemy1的坐标信息是第一行前两个，之后依次为第二行前两个
 Em2_pos = enemy(2,1:2);
 Em3_pos = enemy(3,1:2);
 Em4_pos = enemy(4,1:2);
 Em5_pos = enemy(5,1:2);
 
-% 创建五个友军变量，每个变量包含两个随机数字位置信息
-ally = rand(5, 2);
+% 创建五个友军变量，每个变量包含两个随机数字位置信息。坐标范围是a、b之间。
+%公式 r = a + (b-a).*rand(N,1) 生成区间 (a,b) 内的 N 个随机数。
+ally = -1+(1-(-1)).*rand(5, 2);
+%disp(ally);
 
-% 为每个变量分配一个名字
+% 为每个友军分配一个名字
 al1 = ally(1, :);
 al2 = ally(2, :);
 al3 = ally(3, :);
 al4 = ally(4, :);
 al5 = ally(5, :);
 
+%计算适应度，cost1每行记录的是对于第a个敌人，我方最小距离及对应的我单位索引
 %距离dis1 = norm(al1-Em1_pos);
 %distance = [1,2,3,4,5];
 for a=1:5
-    emCurrent = enemy(a,1:2);
+    emCurrent = enemy(a,1:2);%即Em_pos
     for i=1:5
     distance(a,i)=norm(ally(i,:)-emCurrent);
     %%disp(distance);
