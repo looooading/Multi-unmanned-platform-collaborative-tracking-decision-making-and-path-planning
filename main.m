6ab5c62bee1a7cbddaff706b7f4fad2356c70a71
looooading <1275318444@qq.com>
2024-04-26 15:21:29 +0800

    将随机生成的坐标改为固定范围内生成。还未添加障碍物，基本功能可以实现，图形展示方面仍需精进。

--- main.m
+++ main.m
@@ -2,25 +2,31 @@ function main()
 %%% 相关参数初始化
     disp(fileread('Marx_Engels_Lenin_ASCIIBanner.txt'));
     map = single(zeros(50,50));
-    army_size = 10;
+    army_size = 3;%对称对抗，双方army_size相同
+    decimal = 1;
 
 %%% 创建五个友军变量，每个变量包含两个随机数字位置信息。坐标范围是a、b之间。
     %公式 r = a + (b-a).*rand(N,1) 生成区间 (a,b) 内的 N 个随机数。
-    ally = 20 + (40-20).*rand(army_size, 3);
+    %不再使用随机生成方案 ally = 20 + (40-20).*rand(army_size, 3);
+    ally_x = 10 + (20 - 10).*rand(army_size,1);
+    ally_y = 5 + (20 - 5).*rand(army_size,1);
+    ally = [ally_x ally_y];
     ally(:,3) = zeros(army_size,1);%友军同等价值
-    decimal = 1;
-    ally = single(round(ally * 10^decimal)/10^decimal);
+    ally = single(round(ally * 10^decimal)/10^decimal);%一位小数
     ally_backup = ally;
     %disp(ally);%调试用
     % 为每个友军分配一个名字
     %al1 = ally(1, :);
-    %al2 = ally(2, :);
 
     %创建5个敌人，包含坐标信息与价值信息
     %Em1 = [0.1,0.1,0.1];Em2 = [-0.1,0.1,0.2];Em3 = [-0.1,-0.1,0.1];Em4 = [0.1,-0.1,0.2];Em5 = [0,0,0.3];
     %enemy = [Em1;Em2;Em3;Em4;Em5];%上述生成的敌人信息竖直叠放
     %Em1_pos = enemy(1,1:2);%enemy1的坐标信息是第一行前两个，之后依次为第二行前两个
-    enemy = 10 + (49-10).*rand(army_size,3);enemy(:,3) = rand(army_size,1);
+    %不再使用随机生成方案 enemy = 10 + (49-10).*rand(army_size,3);
+    enemy_x = 30 + (40 - 30).*rand(army_size,1);
+    enemy_y = 35 + (45 - 35).*rand(army_size,1);
+    enemy = [enemy_x enemy_y];
+    enemy(:,3) = rand(army_size,1);
     enemy = single(round(enemy * 10^decimal)/10^decimal);
     enemy_backup = enemy;
 
